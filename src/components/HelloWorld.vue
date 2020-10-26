bc307150a06c0a9cb807421068bc0b1368c9347f
Oktay Ibis <oktayibis@gmail.com>
2020-10-26 20:28:25 +0300

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,229 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <h1>Mesai Saatileri Girişler</h1>
+    <div class="input-group">
+      <form @submit.prevent="send()" class="form">
+        <input
+          :maxlength="11"
+          :minlength="11"
+          required
+          placeholder="TCKN"
+          type="text"
+          v-model="tckn"
+        />
+        <input required placeholder="İsim Soyisim" type="text" v-model="name" />
+        <input
+          required
+          placeholder="Bitirme Tarihi"
+          type="date"
+          v-model="date"
+        />
+        <select required v-model="selected" name="egitimler">
+          <option v-for="name in egitimler" :key="name" :value="name">{{
+            name
+          }}</option>
+        </select>
+        <button class="btn-green" type="submit">Ekle</button>
+      </form>
+    </div>
+    <div class="list">
+      <table>
+        <thead>
+          <th @click="sortBy('tckn')" :class="handleSorted('tckn')">
+            TCKN
+          </th>
+          <th @click="sortBy('name')" :class="handleSorted('name')">
+            İsim Soyisim
+          </th>
+          <th
+            @click="sortBy('selectedCourse')"
+            :class="handleSorted('selectedCourse')"
+          >
+            Eğitim Adı
+          </th>
+          <th @click="sortBy('date')" :class="handleSorted('date')">
+            Bitirme Tarihi
+          </th>
+          <th>
+            Edit
+          </th>
+        </thead>
+        <tbody>
+          <tr v-for="item in allList" :key="item.tckn">
+            <td>{{ item.tckn }}</td>
+            <td>{{ item.name }}</td>
+            <td>{{ item.selectedCourse }}</td>
+            <td>{{ item.date }}</td>
+            <td>
+              <button @click="deleteItem(item.id)" class="btn-red">
+                Delete
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <button class="btn-green" @click="downloadExcel">Excel Olarak İndir</button>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: 'HelloWorld',
+  data() {
+    return {
+      tckn: "",
+      name: "",
+      date: "",
+      selected: "",
+      egitimler: ["Kurs1", "Kurs2", "Kurs3"],
+      allList: [],
+      sortedName: "",
+      asc: true,
+    };
+  },
+
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+
+  methods: {
+    send: function() {
+      const sendItem = {
+        name: this.name,
+        tckn: this.tckn,
+        date: this.date,
+        selectedCourse: this.selected,
+      };
+
+      this.name = "";
+      this.tckn = "";
+      this.date = "";
+      this.selected = "";
+      axios
+        .post("https://vue-js-13b12.firebaseio.com/course.json", {
+          course: sendItem,
+        })
+        .then(() => this.allList.push(sendItem))
+        .catch((error) => console.log(error));
+    },
+    sortBy(name) {
+      if (this.asc) {
+        this.allList.sort((a, b) => a[name].localeCompare(b[name]));
+        this.asc = false;
+      } else {
+        this.allList.sort((a, b) => b[name].localeCompare(a[name]));
+        this.asc = true;
+      }
+      this.sortedName = name;
+    },
+    handleSorted(name) {
+      return name === this.sortedName ? "selectedTh" : null;
+    },
+    downloadExcel() {},
+
+    deleteItem(id) {
+      axios
+        .delete(`https://vue-js-13b12.firebaseio.com/course/${id}.json`)
+        .then(() => {
+          this.allList = this.allList.filter((course) => course.id !== id);
+        });
+    },
+  },
+
+  computed: {},
+
+  created() {
+    axios
+      .get("https://vue-js-13b12.firebaseio.com/course.json")
+      .then((response) => {
+        for (const key in response.data) {
+          const element = {
+            id: key,
+            name: response.data[key].course.name,
+            tckn: response.data[key].course.tckn,
+            date: response.data[key].course.date,
+            selectedCourse: response.data[key].course.selectedCourse,
+          };
+          this.allList.push(element);
+        }
+      });
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
+  text-transform: capitalize;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.input-group,
+.list {
+  border: 1px solid lightgray;
+  border-radius: 10px;
+  width: 80%;
+  margin: 0 auto;
+  padding: 20px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+input,
+select {
+  padding: 10px;
+  background-color: aliceblue;
+  margin: 10px;
+  border: 1px solid aliceblue;
+  width: 20%;
 }
-a {
-  color: #42b983;
+
+input:focus {
+  background-color: whitesmoke;
+  border: 1px solid whitesmoke;
+}
+.btn-green {
+  background-color: green;
+  border: none;
+  padding: 10px 25px;
+  color: white;
+  font-weight: bold;
+  border-radius: 10px;
+  width: 30%;
+  margin: 10px auto;
+}
+.btn-green:hover {
+  background-color: #42b983;
+  cursor: pointer;
+}
+table {
+  width: 100%;
+}
+tr {
+  background-color: whitesmoke;
+  border: 1px solid gray;
+}
+tr:nth-child(even) {
+  background-color: lightgrey;
+}
+th:hover {
+  cursor: pointer;
+  color: lightcoral;
+}
+.selectedTh {
+  color: lightcoral;
+}
+
+.btn-red {
+  background-color: darkred;
+  border: none;
+  padding: 10px 25px;
+  color: white;
+  font-weight: bold;
+  border-radius: 10px;
+  margin: 10px auto;
+}
+.btn-red:hover {
+  background-color: rgb(95, 2, 2);
+  cursor: pointer;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import excel from "vue-excel-export";
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false;
+Vue.use(excel);
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
